4735c983676c647c3ea44fd109dea202e629d513
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-09 09:09:14 +0200

    Update utilities.h
    Changed variable name to match utilities.cpp

--- IFO1 Hausprojekt/utilities.h
+++ IFO1 Hausprojekt/utilities.h
@@ -5,4 +5,4 @@
 void generate_company_email(char email_destination[email_length], char first_name[first_name_length], char surname[surname_length], char company_name[company_name_length]);
 void generate_hs21_email(char email_destination[email_length], char first_name[first_name_length], char surname[surname_length]);
 
-bool search_datapoint(struct student *student, char query[query_length]);
+bool search_datapoint(struct student *student, char _query[query_length]);
