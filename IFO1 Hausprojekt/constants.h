4f73db21e1e25b036e9cf50dc96c88418da360ee
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-09 08:21:32 +0200

    Update constants.h
    Added constants for search functionality

--- IFO1 Hausprojekt/constants.h
+++ IFO1 Hausprojekt/constants.h
@@ -10,3 +10,8 @@ const unsigned short int city_name_length = 32;
 const unsigned short int course_of_study_length = 32;
 
 const unsigned short int email_length = first_name_length + surname_length + company_name_length + 32;
+
+
+
+const unsigned short int query_length = 64;
+const unsigned short int stringified_number_length = 16;
