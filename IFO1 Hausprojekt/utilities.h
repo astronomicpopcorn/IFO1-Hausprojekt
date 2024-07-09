81befda39ec38ecda5c1c8b78211287a7e7b65d7
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-09 11:04:49 +0200

    Update utilities.h

--- IFO1 Hausprojekt/utilities.h
+++ IFO1 Hausprojekt/utilities.h
@@ -6,3 +6,8 @@ void generate_company_email(char email_destination[email_length], char first_nam
 void generate_hs21_email(char email_destination[email_length], char first_name[first_name_length], char surname[surname_length]);
 
 bool search_datapoint(struct student *student, char _query[query_length]);
+
+
+bool get_window_position(Vector2D *topleft, Vector2D *size);
+Vector2D get_mouse_position();
+Vector2D get_mouse_on_window();
