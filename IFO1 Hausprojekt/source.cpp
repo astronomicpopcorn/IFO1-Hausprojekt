ef2727d756f52001018fa350f05e6ff1c6b6b915
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-10 08:51:19 +0200

    Update source.cpp
    Now confors with datastructure changes

--- IFO1 Hausprojekt/source.cpp
+++ IFO1 Hausprojekt/source.cpp
@@ -16,13 +16,13 @@ void main() {
     strcpy(students[0].first_name, "Nick");
     strcpy(students[0].surname, "Winter");
     strcpy(students[0].course_of_study, "Mechatronik");
-    students[0].phone_number = 4915151727981;
-    students[0].enrollment_year = 2023;
-    students[0].student_number = 202050;
+    strcpy(students[0].phone_number, "4915151727981");
+    strcpy(students[0].enrollment_year, "2023");
+    strcpy(students[0].student_number, "202050");
     strcpy(students[0].home_address.city, "Buxtehude");
     strcpy(students[0].home_address.street, "Estebrügger Straße");
     strcpy(students[0].home_address.house_number, "31A");
-    students[0].home_address.postal_code = 21614;
+    strcpy(students[0].home_address.postal_code, "21614");
     strcpy(students[0].company.contact_person.first_name, "Rafael");
     strcpy(students[0].company.contact_person.surname, "Götzen");
     strcpy(students[0].company.name, "Airbus");
@@ -30,24 +30,24 @@ void main() {
     generate_company_email(students[0].company.contact_person.email, students[0].company.contact_person.first_name, students[0].company.contact_person.surname, students[0].company.name);
     generate_hs21_email(students[0].email, students[0].first_name, students[0].surname);
 
-    students[0].company.contact_person.phone_number = 1234567890;
+    strcpy(students[0].company.contact_person.phone_number, "1234567890");
     strcpy(students[0].company.address.city, "Hamburg");
     strcpy(students[0].company.address.street, "Kreetslag");
     strcpy(students[0].company.address.house_number, "10");
-    students[0].company.address.postal_code = 21129;
+    strcpy(students[0].company.address.postal_code, "21129");
 
 
 
     strcpy(students[1].first_name, "Ole");
     strcpy(students[1].surname, "Spindelndreher");
     strcpy(students[1].course_of_study, "Mechatronik");
-    students[1].phone_number = 4915151727981;
-    students[1].enrollment_year = 2023;
-    students[1].student_number = 202050;
+    strcpy(students[1].phone_number, "4915151727981");
+    strcpy(students[1].enrollment_year, "2023");
+    strcpy(students[1].student_number, "02050");
     strcpy(students[1].home_address.city, "Estebrügge");
     strcpy(students[1].home_address.street, "Bahnhofsstraße");
     strcpy(students[1].home_address.house_number, "76");
-    students[1].home_address.postal_code = 21614;
+    strcpy(students[1].home_address.postal_code, "21614");
     strcpy(students[1].company.contact_person.first_name, "Martin");
     strcpy(students[1].company.contact_person.surname, "Möhre");
     strcpy(students[1].company.name, "Airbus");
@@ -55,24 +55,24 @@ void main() {
     generate_company_email(students[1].company.contact_person.email, students[1].company.contact_person.first_name, students[1].company.contact_person.surname, students[1].company.name);
     generate_hs21_email(students[1].email, students[1].first_name, students[1].surname);
 
-    students[1].company.contact_person.phone_number = 739246248;
+    strcpy(students[1].company.contact_person.phone_number, "739246248");
     strcpy(students[1].company.address.city, "Hamburg");
     strcpy(students[1].company.address.street, "Kreetslag");
     strcpy(students[1].company.address.house_number, "10");
-    students[1].company.address.postal_code = 21129;
+    strcpy(students[1].company.address.postal_code, "21129");
 
 
 
     strcpy(students[2].first_name, "Jill Kendra");
     strcpy(students[2].surname, "Hermann");
     strcpy(students[2].course_of_study, "Logistik");
-    students[2].phone_number = 4915151727981;
-    students[2].enrollment_year = 2023;
-    students[2].student_number = 202050;
+    strcpy(students[2].phone_number, "4915151727981");
+    strcpy(students[2].enrollment_year, "2023");
+    strcpy(students[2].student_number, "202050");
     strcpy(students[2].home_address.city, "Hamburg");
     strcpy(students[2].home_address.street, "Linker Weg");
     strcpy(students[2].home_address.house_number, "12");
-    students[2].home_address.postal_code = 21614;
+    strcpy(students[2].home_address.postal_code, "21614");
     strcpy(students[2].company.contact_person.first_name, "Hans");
     strcpy(students[2].company.contact_person.surname, "Heinrich");
     strcpy(students[2].company.name, "Lufthansa");
@@ -80,11 +80,11 @@ void main() {
     generate_company_email(students[2].company.contact_person.email, students[2].company.contact_person.first_name, students[2].company.contact_person.surname, students[2].company.name);
     generate_hs21_email(students[2].email, students[2].first_name, students[2].surname);
 
-    students[2].company.contact_person.phone_number = 8597243543;
+    strcpy(students[2].company.contact_person.phone_number, "8597243543");
     strcpy(students[2].company.address.city, "Hamburg");
     strcpy(students[2].company.address.street, "Flughafenstraße");
     strcpy(students[2].company.address.house_number, "335");
-    students[2].company.address.postal_code = 74239;
+    strcpy(students[2].company.address.postal_code, "74239");
 
 
 
