d2c654dd2fc1dd92557c9335b3a750cab3cf5db4
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-10 08:46:04 +0200

    Update constants.h
    Added new string max lengths after changing datastructure.h

--- IFO1 Hausprojekt/constants.h
+++ IFO1 Hausprojekt/constants.h
@@ -9,12 +9,15 @@ const unsigned short int company_name_length = 32;
 const unsigned short int street_name_length = 32;
 const unsigned short int city_name_length = 32;
 const unsigned short int house_number_length = 8;
+const unsigned short int postal_code_length = 8;
 
 const unsigned short int course_of_study_length = 32;
+const unsigned short int phone_number_length = 32;
+const unsigned short int student_number_length = 8;
+const unsigned short int enrollment_year_length = 8;
 
 const unsigned short int email_length = first_name_length + surname_length + company_name_length + 32;
 
 
 
 const unsigned short int query_length = 64;
-const unsigned short int stringified_number_length = 22;
