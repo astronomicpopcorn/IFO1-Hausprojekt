3da9c8df20dd579f384c7761456a031ecebb1b2e
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-10 08:27:52 +0200

    Update utilities.h

--- IFO1 Hausprojekt/utilities.h
+++ IFO1 Hausprojekt/utilities.h
@@ -12,3 +12,4 @@ bool get_window_position(Vector2D *topleft, Vector2D *size);
 Vector2D get_console_size();
 Vector2D get_mouse_position();
 Vector2D get_mouse_on_window();
+Vector2D get_mouse_on_console();
