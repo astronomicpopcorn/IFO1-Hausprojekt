7ce587a41a9a4e0dc823545b5281fa1926ee474b
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-10 08:22:47 +0200

    Update utilities.h

--- IFO1 Hausprojekt/utilities.h
+++ IFO1 Hausprojekt/utilities.h
@@ -6,9 +6,7 @@ void generate_company_email(char email_destination[email_length], char first_nam
 void generate_hs21_email(char email_destination[email_length], char first_name[first_name_length], char surname[surname_length]);
 
 bool search_datapoint(struct student *student, char _query[query_length]);
-
-
-int map(int value, int sourcerange_min, int sourcerange_max, int targetrange_min, int targetrange_max);
+int search_dataset(student students[dataset_size], char query[query_length], list_of_matching_indices[dataset_size]);
 
 bool get_window_position(Vector2D *topleft, Vector2D *size);
 Vector2D get_console_size();
