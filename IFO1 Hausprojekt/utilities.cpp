e4e0ca88bd8f04f6f49872f654ef0b6f6f20b03f
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-10 08:54:55 +0200

    Update utilities.cpp
    search_datapoint now conforms to datastructure changes

--- IFO1 Hausprojekt/utilities.cpp
+++ IFO1 Hausprojekt/utilities.cpp
@@ -31,41 +31,34 @@ bool search_datapoint(struct student *student, char _query[query_length]){
     char query[query_length];
     strcpy(query, _query); //to not modify original query string
     bool match = false;
-    char stringified_number[stringified_number_length];
     char *token;
     token = strtok(query, " ,.");      //split query into tokens
     while(token != NULL && !match){    //loop over all tokens while no match has been found
         //*student base data
-        sprintf(stringified_number, "%i", student->student_number);
-        match = match || strstr(stringified_number, token) != NULL;
+        match = match || strstr(student->student_number, token) != NULL;
         match = match || strstr(student->first_name, token) != NULL;
         match = match || strstr(student->surname, token) != NULL;
         match = match || strstr(student->email, token) != NULL;
-        sprintf(stringified_number, "%i", student->enrollment_year);
-        match = match || strstr(stringified_number, token) != NULL;
+        match = match || strstr(student->enrollment_year, token) != NULL;
         match = match || strstr(student->course_of_study, token) != NULL;
-        sprintf(stringified_number, "%i", student->phone_number);
-        match = match || strstr(stringified_number, token) != NULL;
+        match = match || strstr(student->phone_number, token) != NULL;
         //*student address
         match = match || strstr(student->home_address.street, token) != NULL;
         match = match || strstr(student->home_address.house_number, token) != NULL;
         match = match || strstr(student->home_address.city, token) != NULL;
-        sprintf(stringified_number, "%i", student->home_address.postal_code);
-        match = match || strstr(stringified_number, token) != NULL;
+        match = match || strstr(student->home_address.postal_code, token) != NULL;
         //*student company
         match = match || strstr(student->company.name, token) != NULL;
         //company address
         match = match || strstr(student->company.address.street, token) != NULL;
         match = match || strstr(student->company.address.house_number, token) != NULL;
         match = match || strstr(student->company.address.city, token) != NULL;
-        sprintf(stringified_number, "%i", student->company.address.postal_code);
-        match = match || strstr(stringified_number, token) != NULL;
+        match = match || strstr(student->company.address.postal_code, token) != NULL;
         //company contact_person
         match = match || strstr(student->company.contact_person.first_name, token) != NULL;
         match = match || strstr(student->company.contact_person.surname, token) != NULL;
         match = match || strstr(student->company.contact_person.email, token) != NULL;
-        sprintf(stringified_number, "%i", student->company.contact_person.phone_number);
-        match = match || strstr(stringified_number, token) != NULL;
+        match = match || strstr(student->company.contact_person.phone_number, token) != NULL;
 
         //Update Token
         token = strtok(NULL, " ,.");
