40f01a9e204b809245ff979385927b1c214e23c2
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-11 14:33:45 +0200

    Added function to print student

--- IFO1 Hausprojekt/utilities.cpp
+++ IFO1 Hausprojekt/utilities.cpp
@@ -3,6 +3,7 @@
 
 #include <ctype.h>
 #include <windows.h>
+#include <stdio.h>
 
 void lowercase_string(char *ptr){
     while (*ptr) {
@@ -13,4 +14,21 @@ void lowercase_string(char *ptr){
 
 bool is_mouse_pressed(){
     return GetAsyncKeyState(VK_LBUTTON) != 0;
+}
+
+
+void print_student(student student){
+    printf("%s %s\n", student.first_name, student.surname);
+    printf("(%s, Jahrgang %s)\n", student.course_of_study, student.enrollment_year);
+    printf("%s\n", student.email);
+    printf("%s\n", student.phone_number);
+    printf("%s %s\n", student.home_address.street, student.home_address.house_number);
+    printf("%s %s\n", student.home_address.postal_code, student.home_address.city);
+    printf("\nPraxispartner: %s\n", student.company.name);
+    printf("%s %s\n", student.company.address.street, student.company.address.house_number);
+    printf("%s %s\n", student.company.address.postal_code, student.company.address.city);
+    printf("Kontaktperson:\n");
+    printf("%s %s\n", student.company.contact_person.first_name, student.company.contact_person.surname);
+    printf("%s\n", student.company.contact_person.email);
+    printf("%s\n", student.company.contact_person.phone_number);
 }
