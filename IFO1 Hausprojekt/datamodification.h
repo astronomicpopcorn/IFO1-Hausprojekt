b56473e4120aa4dfab7797dbc3e9b54787a5589d
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-12 09:40:20 +0200

    Update datamodification.h
    Change surname to last_name

--- IFO1 Hausprojekt/datamodification.h
+++ IFO1 Hausprojekt/datamodification.h
@@ -3,8 +3,8 @@
 #include "datastructures.h"
 
 
-void generate_company_email(char email_destination[email_length], char first_name[first_name_length], char surname[surname_length], char company_name[company_name_length]);
-void generate_hs21_email(char email_destination[email_length], char first_name[first_name_length], char surname[surname_length]);
+void generate_company_email(char email_destination[email_length], char first_name[first_name_length], char last_name[last_name_length], char company_name[company_name_length]);
+void generate_hs21_email(char email_destination[email_length], char first_name[first_name_length], char last_name[last_name_length]);
 
 student empty_datapoint();
 student enter_datapoint();
