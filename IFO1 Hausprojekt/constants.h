50ffede46ed228146f3a198a9a0b0b85c6b554ed
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-12 09:39:03 +0200

    Update constants.h
    change surname to last_name

--- IFO1 Hausprojekt/constants.h
+++ IFO1 Hausprojekt/constants.h
@@ -3,7 +3,7 @@
 const unsigned short int dataset_size = 150;
 
 const unsigned short int first_name_length = 32;
-const unsigned short int surname_length = 32;
+const unsigned short int last_name_length = 32;
 const unsigned short int company_name_length = 32;
 
 const unsigned short int street_name_length = 32;
@@ -16,7 +16,7 @@ const unsigned short int phone_number_length = 32;
 const unsigned short int student_number_length = 8;
 const unsigned short int enrollment_year_length = 8;
 
-const unsigned short int email_length = first_name_length + surname_length + company_name_length + 32;
+const unsigned short int email_length = first_name_length + last_name_length + company_name_length + 32;
 
 
 
