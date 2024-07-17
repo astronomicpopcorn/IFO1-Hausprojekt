b155cd7a662cb513e5c9ac50cc38fc5fa9bc1828
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-07-17 08:23:38 +0200

    Added replace_whitespace

--- IFO1 Hausprojekt/utilities.cpp
+++ IFO1 Hausprojekt/utilities.cpp
@@ -130,7 +130,13 @@ void render_something(button buttonlist[button_list_length]){
     printf("%s%s", "\033[H", frame);
 }
 
-
+void replace_whitespace(const char* string_to_change, char replacement){
+    for(int i = 0; string_to_change[i] != '\0'; i++){
+        if(string_to_change[i] == ' '){
+            string_to_change[i] = replacement;
+        }
+    }
+}
 
 void replace_umlaute(char mystring[32], char buffer[64]){
     int buffer_pointer = 0;
