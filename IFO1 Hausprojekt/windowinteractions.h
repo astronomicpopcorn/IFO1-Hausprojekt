43d72de7c848891a9f51a7bb7ac31c12340ecf30
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-08-05 09:21:20 +0200

    Update windowinteractions.h

--- IFO1 Hausprojekt/windowinteractions.h
+++ IFO1 Hausprojekt/windowinteractions.h
@@ -2,8 +2,8 @@
 #include "datastructures.h"
 
 
-bool get_window_position(Vector2D *topleft, Vector2D *size);
-Vector2D get_console_size();
-Vector2D get_mouse_position();
-Vector2D get_mouse_on_window();
-Vector2D get_mouse_on_console();
+extern bool get_window_position(Vector2D *topleft, Vector2D *size);
+extern Vector2D get_console_size();
+extern Vector2D get_mouse_position();
+extern Vector2D get_mouse_on_window();
+extern Vector2D get_mouse_on_console();
