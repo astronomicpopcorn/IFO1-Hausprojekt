aec5878a389443f3e0a6528ed1e1546966f27776
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-08-05 09:20:35 +0200

    Update utilities.h

--- IFO1 Hausprojekt/utilities.h
+++ IFO1 Hausprojekt/utilities.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "datastructures.h"
 
-void lowercase_string(char *ptr);
-bool is_mouse_pressed();
-void print_student(student student);
+extern bool async_scanf(char buffer[async_input_length], char character_to_check);
 
-bool save_file(student list[dataset_size], const char* filename);
-bool load_file(student list[dataset_size], const char* filename);
-void add_file_extension(char filename[filename_length]);
+extern void lowercase_string(char *ptr);
+extern bool is_mouse_pressed();
+extern void print_student(student student);
 
-void replace_whitespace(char* string_to_change, char replacement);
+extern bool save_file(student list[dataset_size], const char* filename);
+extern bool load_file(student list[dataset_size], const char* filename);
+extern void add_file_extension(char filename[filename_length]);
 
-void render_something(button buttonlist[button_list_length]);
+extern void replace_whitespace(char* string_to_change, char replacement);
 
-bool select_next_button(button buttonlist[button_list_length], char direction);
+extern void render_something(button buttonlist[button_list_length]);
+
+extern bool select_next_button(button buttonlist[button_list_length], char direction);
