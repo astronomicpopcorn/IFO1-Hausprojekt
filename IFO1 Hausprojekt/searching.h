65ec3710f22b53a91eb70f28837e48d6c8d14099
astronomicpopcorn <130027969+astronomicpopcorn@users.noreply.github.com>
2024-08-05 09:20:45 +0200

    Update searching.h

--- IFO1 Hausprojekt/searching.h
+++ IFO1 Hausprojekt/searching.h
@@ -3,5 +3,5 @@
 #include "datastructures.h"
 
 
-bool search_datapoint(struct student *student, char _query[query_length]);
-int search_dataset(student students[dataset_size], char query[query_length], short int list_of_matching_indices[dataset_size]);
+extern bool search_datapoint(struct student *student, char _query[query_length]);
+extern int search_dataset(student students[dataset_size], char query[query_length], short int list_of_matching_indices[dataset_size]);
