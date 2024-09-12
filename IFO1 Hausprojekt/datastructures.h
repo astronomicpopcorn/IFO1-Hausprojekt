cb6d3b9efef3862bc4e5215710e458bc9f3b151a
Nick Winter <nickhatpost@gmail.com>
2024-09-12 20:48:31 +0200

    fix typo

--- IFO1 Hausprojekt/datastructures.h
+++ IFO1 Hausprojekt/datastructures.h
@@ -35,7 +35,7 @@ typedef struct student {
     char student_number[student_number_length] = "\0";
     char first_name[first_name_length] = "\0";
     char last_name[last_name_length] = "\0";
-    char email[email_length] = "/0";
+    char email[email_length] = "\0";
     char enrollment_year[enrollment_year_length] = "\0";
     char course_of_study[course_of_study_length] = "\0";
     address home_address;
